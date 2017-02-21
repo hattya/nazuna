297ea28cd3f081939af88867ebbb409c9085288d
Akinori Hattori <hattya@gmail.com>
2017-02-21 22:11:06 +0900

    Refine

--- util_windows.go
+++ util_windows.go
@@ -41,12 +41,16 @@ func IsLink(path string) bool {
 		return false
 	}
 	defer windows.CloseHandle(h)
-	// hardlink
+
 	var fi windows.ByHandleFileInformation
-	if err := windows.GetFileInformationByHandle(h, &fi); err == nil && 1 < fi.NumberOfLinks {
+	if err := windows.GetFileInformationByHandle(h, &fi); err != nil {
+		return false
+	}
+	// hard link
+	if 1 < fi.NumberOfLinks {
 		return true
 	}
-	// junction
+	// reparse point
 	if fi.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT == 0 {
 		return false
 	}
@@ -56,8 +60,8 @@ func IsLink(path string) bool {
 		return false
 	}
 	switch rdb := (*reparseDataBuffer)(unsafe.Pointer(&b[0])); rdb.ReparseTag {
-	case windows.IO_REPARSE_TAG_MOUNT_POINT:
 	case windows.IO_REPARSE_TAG_SYMLINK:
+	case windows.IO_REPARSE_TAG_MOUNT_POINT:
 	default:
 		return false
 	}
@@ -70,26 +74,27 @@ func LinksTo(path, origin string) bool {
 		return false
 	}
 	defer windows.CloseHandle(h)
-	// hardlink
-	var fi windows.ByHandleFileInformation
-	switch err := windows.GetFileInformationByHandle(h, &fi); {
-	case err != nil:
+
+	var fi1 windows.ByHandleFileInformation
+	if err := windows.GetFileInformationByHandle(h, &fi1); err != nil {
 		return false
-	case 1 < fi.NumberOfLinks:
+	}
+	// hard link
+	if 1 < fi1.NumberOfLinks {
 		h, err := createFile(origin, 0)
 		if err != nil {
 			return false
 		}
 		defer windows.CloseHandle(h)
 
-		var ofi windows.ByHandleFileInformation
-		if err := windows.GetFileInformationByHandle(h, &ofi); err != nil || ofi.NumberOfLinks == 1 {
+		var fi2 windows.ByHandleFileInformation
+		if err := windows.GetFileInformationByHandle(h, &fi2); err != nil {
 			return false
 		}
-		return fi.VolumeSerialNumber == ofi.VolumeSerialNumber && fi.FileIndexHigh == ofi.FileIndexHigh && fi.FileIndexLow == ofi.FileIndexLow
+		return fi1.VolumeSerialNumber == fi2.VolumeSerialNumber && fi1.FileIndexHigh == fi2.FileIndexHigh && fi1.FileIndexLow == fi2.FileIndexLow
 	}
-	// junction
-	if fi.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT == 0 {
+	// reparse point
+	if fi1.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT == 0 {
 		return false
 	}
 	path, err = filepath.Abs(path)
@@ -106,74 +111,36 @@ func LinksTo(path, origin string) bool {
 		return false
 	}
 	switch rdb := (*reparseDataBuffer)(unsafe.Pointer(&b[0])); rdb.ReparseTag {
-	case windows.IO_REPARSE_TAG_MOUNT_POINT:
-		rb := (*mountPointReparseBuffer)(unsafe.Pointer(&rdb.ReparseBuffer))
-		start := rb.SubstituteNameOffset / 2
-		end := start + rb.SubstituteNameLength/2
-		path = windows.UTF16ToString((*[0xffff]uint16)(unsafe.Pointer(&rb.PathBuffer[0]))[start:end])
 	case windows.IO_REPARSE_TAG_SYMLINK:
 		rb := (*symbolicLinkReparseBuffer)(unsafe.Pointer(&rdb.ReparseBuffer))
-		start := rb.SubstituteNameOffset / 2
-		end := start + rb.SubstituteNameLength/2
-		p := windows.UTF16ToString((*[0xffff]uint16)(unsafe.Pointer(&rb.PathBuffer[0]))[start:end])
+		pb := (*[0xffff]uint16)(unsafe.Pointer(&rb.PathBuffer[0]))
+		p := windows.UTF16ToString(pb[rb.SubstituteNameOffset/2 : (rb.SubstituteNameOffset+rb.SubstituteNameLength)/2])
 		if rb.Flags&_SYMLINK_FLAG_RELATIVE != 0 {
 			path = filepath.Join(filepath.Dir(path), p)
 		} else {
 			path = p
 		}
+	case windows.IO_REPARSE_TAG_MOUNT_POINT:
+		rb := (*mountPointReparseBuffer)(unsafe.Pointer(&rdb.ReparseBuffer))
+		pb := (*[0xffff]uint16)(unsafe.Pointer(&rb.PathBuffer[0]))
+		path = windows.UTF16ToString(pb[rb.SubstituteNameOffset/2 : (rb.SubstituteNameOffset+rb.SubstituteNameLength)/2])
 	default:
 		return false
 	}
 	if strings.HasPrefix(path, `\??\`) {
 		path = path[4:]
+		switch {
+		case 2 <= len(path) && path[1] == ':':
+		case 4 <= len(path) && path[:4] == `UNC\`:
+			path = `\\` + path[4:]
+		}
 	}
 	return path == origin
 }
 
 func CreateLink(src, dst string) error {
-	linkError := func(err error) error {
-		return &os.LinkError{
-			Op:  "link",
-			Old: src,
-			New: dst,
-			Err: err,
-		}
-	}
-
 	if IsDir(src) {
-		if _, err := os.Stat(dst); err == nil {
-			return linkError(windows.ERROR_ALREADY_EXISTS)
-		}
-		if err := os.MkdirAll(dst, 0777); err != nil {
-			return err
-		}
-		h, err := createFile(dst, windows.GENERIC_WRITE)
-		if err != nil {
-			return linkError(err)
-		}
-		defer windows.CloseHandle(h)
-
-		path, err := filepath.Abs(src)
-		if err != nil {
-			return err
-		}
-		sn, _ := windows.UTF16FromString(`\??\` + path)
-		pn, _ := windows.UTF16FromString(path)
-
-		b := make([]byte, windows.MAXIMUM_REPARSE_DATA_BUFFER_SIZE)
-		var retlen uint32
-		rdb := (*reparseDataBuffer)(unsafe.Pointer(&b[0]))
-		rdb.ReparseTag = windows.IO_REPARSE_TAG_MOUNT_POINT
-		rb := (*mountPointReparseBuffer)(unsafe.Pointer(&rdb.ReparseBuffer))
-		rb.SubstituteNameLength = uint16((len(sn) - 1) * 2)
-		rb.PrintNameOffset = rb.SubstituteNameLength + 2
-		rb.PrintNameLength = uint16((len(pn) - 1) * 2)
-		copy((*[0xffff]uint16)(unsafe.Pointer(&rb.PathBuffer[0]))[:], append(sn, pn...))
-		rdb.ReparseDataLength = 8 + rb.PrintNameOffset + rb.PrintNameLength + 2
-		if err := windows.DeviceIoControl(h, _FSCTL_SET_REPARSE_POINT, &b[0], uint32(rdb.ReparseDataLength+8), nil, 0, &retlen, nil); err != nil {
-			return linkError(err)
-		}
-		return nil
+		return createMountPoint(src, dst)
 	}
 	return os.Link(src, dst)
 }
@@ -195,4 +162,55 @@ func createFile(path string, access uint32) (windows.Handle, error) {
 		return windows.InvalidHandle, err
 	}
 	return windows.CreateFile(p, access, windows.FILE_SHARE_READ|windows.FILE_SHARE_WRITE, nil, windows.OPEN_EXISTING, windows.FILE_FLAG_BACKUP_SEMANTICS|windows.FILE_FLAG_OPEN_REPARSE_POINT, 0)
+}
+
+func createMountPoint(src, dst string) error {
+	linkErr := func(err error) error {
+		return &os.LinkError{
+			Op:  "link",
+			Old: src,
+			New: dst,
+			Err: err,
+		}
+	}
+
+	if _, err := os.Stat(dst); err == nil {
+		return linkErr(windows.ERROR_ALREADY_EXISTS)
+	}
+	if err := os.MkdirAll(dst, 0777); err != nil {
+		return linkErr(err)
+	}
+	h, err := createFile(dst, windows.GENERIC_WRITE)
+	if err != nil {
+		return linkErr(err)
+	}
+	defer windows.CloseHandle(h)
+
+	path, err := filepath.Abs(src)
+	if err != nil {
+		return linkErr(err)
+	}
+	sn, err := windows.UTF16FromString(`\??\` + path)
+	if err != nil {
+		return linkErr(err)
+	}
+	pn, err := windows.UTF16FromString(path)
+	if err != nil {
+		return linkErr(err)
+	}
+
+	b := make([]byte, windows.MAXIMUM_REPARSE_DATA_BUFFER_SIZE)
+	var retlen uint32
+	rdb := (*reparseDataBuffer)(unsafe.Pointer(&b[0]))
+	rdb.ReparseTag = windows.IO_REPARSE_TAG_MOUNT_POINT
+	rb := (*mountPointReparseBuffer)(unsafe.Pointer(&rdb.ReparseBuffer))
+	rb.SubstituteNameLength = uint16((len(sn) - 1) * 2)
+	rb.PrintNameOffset = rb.SubstituteNameLength + 2
+	rb.PrintNameLength = uint16((len(pn) - 1) * 2)
+	copy((*[0xffff]uint16)(unsafe.Pointer(&rb.PathBuffer[0]))[:], append(sn, pn...))
+	rdb.ReparseDataLength = 8 + rb.PrintNameOffset + rb.PrintNameLength + 2
+	if err := windows.DeviceIoControl(h, _FSCTL_SET_REPARSE_POINT, &b[0], uint32(rdb.ReparseDataLength+8), nil, 0, &retlen, nil); err != nil {
+		return linkErr(err)
+	}
+	return nil
 }
